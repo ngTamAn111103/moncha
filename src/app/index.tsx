e9b0bde77de3182edeae0a337941d66472c011e8
ngTamAn111103 <ng.t.ann2003@gmail.com>
2026-09-22 13:40:56 +0700

    Cài đặt mã màu theo theme Montra - Expense Tracker UI Kit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="mt-4 flex-1 items-center justify-center rounded-xl bg-white px-4">
-      <Text className="text-xl font-bold text-blue-500">
+    <View className="mt-4 flex-1 items-center justify-center rounded-xl bg-background-secondary px-4">
+      <Text className="text-xl font-bold text-primary">
         Welcome to Nativewind!
       </Text>
     </View>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,57 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Brand
+        primary: {
+          DEFAULT: "#7F3DFF",
+          background: "#EEE5FF",
+        },
+
+        // Semantic
+        income: {
+          DEFAULT: "#00A86B",
+          background: "#CFFAEA",
+        },
+        expense: {
+          DEFAULT: "#FD3C4A",
+          background: "#FDD5D7",
+        },
+        transfer: {
+          DEFAULT: "#0077FF",
+          background: "#BDDCFF",
+        },
+        warning: {
+          DEFAULT: "#FCAC12",
+          background: "#FCEED4",
+        },
+
+        // Background
+        background: {
+          DEFAULT: "#FFFFFF",
+          secondary: "#FCFCFC",
+        },
+        surface: {
+          DEFAULT: "#FFFFFF",
+          muted: "#F1F1FA",
+        },
+
+        // Text
+        text: {
+          primary: "#292B2D",
+          secondary: "#91919F",
+          inverse: "#FFFFFF",
+        },
+
+        // Border
+        border: "#E3E5E5",
+
+        // Base
+        black: "#0D0E0F",
+        white: "#FFFFFF",
+      },
+    },
   },
   plugins: [],
 }
