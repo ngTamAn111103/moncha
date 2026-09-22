c711a02d3a7ad0fe8c2cff666e0f8dc1149d74d5
ngTamAn111103 <ng.t.ann2003@gmail.com>
2026-09-22 13:06:18 +0700

    Cài đặt thành công Automatic Class Sorting with Prettier

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className="mt-4 flex-1 items-center justify-center rounded-xl bg-white px-4">
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
